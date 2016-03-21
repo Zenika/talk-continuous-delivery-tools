68416c9623f3ce2489c6da96d5f1aa6b17fdcdba
srevereault <sylvain.revereault@gmail.com>
2016-03-21 13:06:16 +0100

    la vm tools devient ci-tools

--- kvm/populate_hosts.sh
+++ kvm/populate_hosts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for host in {tc-prod-1,tc-prod-2,test,db,rp,tools} 
+for host in {tc-prod-1,tc-prod-2,test,db,rp,ci-tools} 
 do virsh domifaddr $host | grep vnet | awk '{ print $4 }' | awk -F/ -v HOST="$host" '{ print $1 " " HOST}' 
 done
